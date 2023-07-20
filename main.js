f992856b80d584abbda3dcab9045bc4e605f85d4
lathamadakkannu <lathamadakkannu@gmail.com>
2023-07-19 19:39:01 -0700

    addnew

---
+++ main.js
@@ -0,0 +1,9 @@
+var arr=[11,10,70,75,59,2,9];
+document.write("arr=[11,10,70,75,59,2,9]" + "<br>" + "<br>");
+
+	for(let i=0; i<arr.length; i++){
+		if(arr[i]>9 &&arr[i]<99){
+			document.write(arr[i])
+		document.write("<br>");
+	}
+	}
